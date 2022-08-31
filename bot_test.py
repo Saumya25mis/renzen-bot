aa85e69639cf724a448857ad18bbe1ff8371d6f3
Erick Tiddle <erickrtiddle@gmail.com>
2022-08-31 15:09:31 -0700

    commit

---
+++ bot_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestApp(unittest.TestCase):
+    """Unit tests for bot."""
+
+    def test_app(self):
+        return True
+
+if __name__ == "__main__":
+    unittest.main()
