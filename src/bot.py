2baecb6764dc370343c8f7c8d39498a876b796df
Erick Tiddle <erickrtiddle@gmail.com>
2022-09-01 16:48:43 -0700

    simpler events, logging

--- src/bot.py
+++ src/bot.py
@@ -2,10 +2,11 @@
 
 """Discord Bot."""
 
-import random
+import logging
 import discord
 from src import get_secret
 
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 
 SECRETS = get_secret.get_secret()
 TOKEN = SECRETS["eklie-token"]
@@ -13,38 +14,24 @@ GUILD = SECRETS["eklie-guild"]
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
-print("got client")
+
+logging.info("got client")
 
 
 @client.event
 async def on_ready():
-    """On Ready client action."""
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f"{client.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})"
-    )
+    """On Ready event."""
+    logging.info("We have logged in as %s", client)
 
 
 @client.event
 async def on_message(message):
-    """On Message client action."""
-    print("got message")
+    """On message event."""
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        "I'm the human form of the 💯 emoji.",
-        "Bingpot!",
-        (
-            "Cool. Cool cool cool cool cool cool cool, "
-            "no doubt no doubt no doubt no doubt."
-        ),
-    ]
-
-    if message.content == "99!":
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
 
 
-client.run(TOKEN)
+client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
