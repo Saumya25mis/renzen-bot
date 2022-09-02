516ea7013e5158519734fe6b5a7f084c31abc379
Erick Tiddle <erickrtiddle@gmail.com>
2022-09-01 22:11:06 -0700

    add bot

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,8 @@
 
 import logging
 import discord
+from discord.ext import commands
+
 from src import get_secret
 
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
@@ -36,3 +38,24 @@ intents.message_content = True
 
 client = MyClient(intents=intents)
 client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+
+
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f"No, {ctx.subcommand_passed} is not cool")
+
+
+@cool.command(name="bot")
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send("Yes, the bot is cool.")
+
+
+bot.run(TOKEN)
