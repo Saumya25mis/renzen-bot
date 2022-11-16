7a7463ee67c8f837380cba720ea8de59623c9929
renadvent <ericktiddle@gmail.com>
2022-11-16 22:34:38 +0000

    add main

--- src/flask_test.py
+++ src/flask_test.py
@@ -1,6 +1,5 @@
-"""Test Response."""
-
 # pylint: disable=import-error
+"""Test Response."""
 
 from flask import Flask
 
@@ -11,4 +10,8 @@ app.config["SECRET_KEY"] = "temp--secret--key"
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     """Return Hello World."""
-    return "Hello World"
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
