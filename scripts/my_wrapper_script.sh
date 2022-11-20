72362f2c3e9421b1af1eec5b92a4887310e1ccf9
renadvent <ericktiddle@gmail.com>
2022-11-20 22:33:51 +0000

    try this way

--- scripts/my_wrapper_script.sh
+++ scripts/my_wrapper_script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-chmod +x ../src/bot.py
-chmod +x ../src/bot_health_check.py
+chmod +x src/bot.py
+chmod +x src/bot_health_check.py
 
 ./src/bot.py &
-
 ./src/bot_health_check.py &
 
 wait -n
