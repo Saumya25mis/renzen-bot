1fd8838966101ad58a5653b1b15fe25b5151c4a7
renadvent <ericktiddle@gmail.com>
2022-11-23 20:06:23 +0000

    move code to site server

--- src/flask_test.py
+++ src/flask_test.py
@@ -1,26 +1,47 @@
 # pylint: disable=import-error
-"""Test Response."""
+"""Expose endpoint for flask server"""
 
-from flask import Flask
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "temp--secret--key"
-
-
-@app.route("/")
-def hello_world():
-    """Return Hello World."""
-    return "<h1>Hello World</h1>", 200
+import json
+import uuid
+import boto3
+from aiohttp import web
 
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
-def catch_all(path):  # pylint: disable=unused-argument
-    """Catch all requests."""
-    return "<h1>Caught It!</h1>", 200
+async def handle(request):  # pylint:disable=unused-argument
+    """Health check response."""
+    response_obj = {"status": "success"}
+    return web.Response(text=json.dumps(response_obj))
 
 
-if __name__ == "__main__":
-    # WEBSITE_URL = "renzen.io:80"  # for subdomain support
-    # app.config["SERVER_NAME"] = WEBSITE_URL
-    app.run(debug=False, port=80, host="0.0.0.0")
+async def forward(request):  # pylint:disable=unused-argument
+    """forward check response."""
+    request_id = uuid.uuid4()
+    send_message(message={"request_content": request_id})
+    # send_message(message = {"request_content": request.text()})
+    response_obj = {"status": "success"}
+    return web.Response(text=json.dumps(response_obj))
+
+
+def get_queue_url():
+    """Get Queue Url."""
+    sqs_client = boto3.client("sqs", region_name="us-west-2")
+    response = sqs_client.get_queue_url(
+        QueueName="MyQueue.fifo",
+    )
+    return response["QueueUrl"]
+
+
+def send_message(message):
+    """Send message to queue."""
+    sqs_client = boto3.client("sqs", region_name="us-west-2")
+    response = sqs_client.send_message(
+        QueueUrl=get_queue_url(), MessageBody=json.dumps(message)
+    )
+    print(response)
+
+
+app = web.Application()
+app.router.add_get("/", handle)
+app.router.add_get("/forward", forward)
+
+web.run_app(app, port=80, host="0.0.0.0")
