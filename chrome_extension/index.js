df3239a3a208307311ed74f951e9ee7f515392b1
renadvent <ericktiddle@gmail.com>
2022-11-24 23:50:53 -0800

    forward url

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -9,7 +9,7 @@ document.getElementById("save-btn").onclick = async () => {
 
       fetch('http://myLoadBalancerSite-2024255806.us-west-1.elb.amazonaws.com/forward', {method: 'POST',   headers: {
         'Content-Type': 'application/json;charset=utf-8'
-      }, body: JSON.stringify({"snippet":result})}).then(r => r.text()).then(result2 => {
+      }, body: JSON.stringify({"snippet":result, "URL": tab.url})}).then(r => r.text()).then(result2 => {
         // Result now contains the response text, do what you want...
         document.body.append(result2);
     })
