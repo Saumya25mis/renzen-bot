311305a360189184901dc2922d4e6e9d543acb06
renadvent <ericktiddle@gmail.com>
2022-11-24 22:45:13 -0800

    temp chrome forwarding

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -6,6 +6,12 @@ document.getElementById("save-btn").onclick = async () => {
         target: {tabId: tab.id},
         function: () => getSelection().toString(),
       });
+
+      fetch('http://myLoadBalancerSite-2024255806.us-west-1.elb.amazonaws.com/forward').then(r => r.text()).then(result2 => {
+        // Result now contains the response text, do what you want...
+        document.body.append(result2);
+    })
+
     } catch (e) {
       return; // ignoring an unsupported page like chrome://extensions
     }
