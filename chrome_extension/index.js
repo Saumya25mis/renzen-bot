c7f476c3a985885ccf8dbf89b4c2dc08a06a170b
renadvent <ericktiddle@gmail.com>
2022-11-25 19:05:18 +0000

    use domain name

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -12,9 +12,12 @@ document.getElementById("save-btn").onclick = async () => {
         function: () => getSelection().toString(),
       });
 
+      let site = "http://api.renzen.io/forward"
+
       chrome.storage.sync.get(['login-code'], function(login_result) {
         // console.log('Value currently is ' + result.key);
-        fetch('http://myLoadBalancerSite-2024255806.us-west-1.elb.amazonaws.com/forward', {method: 'POST',   headers: {
+        fetch(site, {method: 'POST',   headers: {
+        // fetch('http://myLoadBalancerSite-2024255806.us-west-1.elb.amazonaws.com/forward', {method: 'POST',   headers: {
           'Content-Type': 'application/json;charset=utf-8'
         }, body: JSON.stringify({"snippet":result, "login-code": login_result['login-code'], "URL": tab.url, })}).then(r => r.text()).then(result2 => {
           // Result now contains the response text, do what you want...
