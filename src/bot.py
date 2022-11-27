34e724b8bbfee82544a38ec9864f8d9fabf4470a
renadvent <ericktiddle@gmail.com>
2022-11-27 00:44:16 +0000

    switch to followup

--- src/bot.py
+++ src/bot.py
@@ -32,7 +32,8 @@ queue = sqs.get_queue_by_name(QueueName="MyQueue.fifo")
 @my_bot.tree.command()
 async def get_code(interaction: discord.Interaction):
     """Slash command to get chrome extension code."""
-    await interaction.response.send_message("code command ack")
+    await interaction.followup.send("code command ack")
+    # await interaction.response.send_message("code command ack")
     key = db_utils.create_code(interaction.user.id, interaction.user.display_name)
     await interaction.response.send_message(f"Your key is: {key}")
     return
@@ -41,7 +42,7 @@ async def get_code(interaction: discord.Interaction):
 @my_bot.tree.command()
 async def invalidate_codes(interaction: discord.Interaction):
     """Test command. Prints what follows `!test`. ex: `!test hi`"""
-    await interaction.response.send_messaged("invalidate_codes command ack")
+    await interaction.followup.send("invalidate_codes command ack")
     db_utils.invalidate_codes(interaction.user.id)
     await interaction.response.send_message("All codes have been invalidated")
     return
