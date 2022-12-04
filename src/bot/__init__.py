f080f776b1ae977282a9ad1afcd3e79380424477
renadvent <ericktiddle@gmail.com>
2022-12-04 00:05:12 +0000

    remove logging customization for now

--- src/bot/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Common resources."""
-import logging
-
-logging.basicConfig(
-    format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%Y-%m-%d:%H:%M:%S",
-    level=logging.DEBUG,
-)
