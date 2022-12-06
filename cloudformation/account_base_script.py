a09995146fb110c373b85dbb1e8cc8360c4e8485
renadvent <ericktiddle@gmail.com>
2022-12-06 19:43:52 +0000

    putting files at wrong spot

--- cloudformation/account_base_script.py
+++ cloudformation/account_base_script.py
@@ -7,7 +7,7 @@ import boto3
 import s3fs  # type: ignore
 
 s3_file = s3fs.S3FileSystem()
-local_path = "cloudformation/"
+local_path = "cloudformation"
 s3_path = "s3://cloudformation-files-renzen/cloudformation/"
 s3_file.rm(s3_path, recursive=True, maxdepth=10)
 s3_file.put(local_path, s3_path, recursive=True)
