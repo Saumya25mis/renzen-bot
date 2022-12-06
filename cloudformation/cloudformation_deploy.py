9c6012b793b176c93572afcb4d86103975b40ade
renadvent <ericktiddle@gmail.com>
2022-12-06 23:51:56 +0000

    fix stack file names

--- cloudformation/cloudformation_deploy.py
+++ cloudformation/cloudformation_deploy.py
@@ -21,36 +21,41 @@ stacks_order: List[str] = [
 stacks_name_compliant = [
     file.replace("_", "").replace(".yml", "") for file in stacks_order
 ]
-print(f"Creating or Updating: {stacks_name_compliant}")
+
+stacks_zipped = zip(stacks_order, stacks_name_compliant)
+
+print(f"Creating or Updating: {stacks_zipped}")
 
 # get cloudformation client to get current stacks
 cloudformation_client = boto3.client("cloudformation")
 
-for stack_name in stacks_name_compliant:
+for stack_name, stack_name_compliant in stacks_zipped:
 
     try:
-        stack_summary = cloudformation_client.describe_stacks(StackName=stack_name)
+        stack_summary = cloudformation_client.describe_stacks(
+            StackName=stack_name_compliant
+        )
         print(f"{stack_summary=}")
         # stack exists, so attempt to update
-        print(f"Stack exists. Attempting to update {stack_name}...")
+        print(f"Stack exists. Attempting to update {stack_name_compliant}...")
         update_response = cloudformation_client.update_stack(
-            StackName=stack_name,
+            StackName=stack_name_compliant,
             Capabilities=["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"],
             TemplateURL=f"{stack_prefix}/{stack_name}",
         )
         cloudformation_client.get_waiter("stack_update_complete").wait(
-            StackName=stack_name
+            StackName=stack_name_compliant
         )
-        print(f"Updated: {stack_name}")
+        print(f"Updated: {stack_name_compliant}")
     except (botocore.exceptions.ValidationError, botocore.exceptions.ClientError) as e:
         # stack does NOT, so attempt to create
-        print(f"Stack does not exist. Attempting to create {stack_name}")
+        print(f"Stack does not exist. Attempting to create {stack_name_compliant}")
         create_response = cloudformation_client.create_stack(
-            StackName=stack_name,
+            StackName=stack_name_compliant,
             Capabilities=["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"],
             TemplateURL=f"{stack_prefix}/{stack_name}",
         )
         cloudformation_client.get_waiter("stack_create_complete").wait(
-            StackName=stack_name
+            StackName=stack_name_compliant
         )
-        print(f"Created: {stack_name}")
+        print(f"Created: {stack_name_compliant}")
